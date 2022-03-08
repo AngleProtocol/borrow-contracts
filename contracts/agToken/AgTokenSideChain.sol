17094b5644c8ef1a4cb48e9c77982ab3bed59835
Pablo Veyrat <veyratpablo@gmail.com>
2022-03-08 19:54:25 +0100

    adding agToken rinkeby, tests to be added

--- contracts/agToken/AgTokenSideChain.sol
+++ contracts/agToken/AgTokenSideChain.sol
@@ -14,61 +14,40 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20Pe
 /// @dev This contract is used to create and handle the stablecoins of Angle protocol
 /// @dev It is still possible for any address to burn its agTokens without redeeming collateral in exchange
 /// @dev This contract is the upgraded version of the AgToken that was first deployed on Ethereum mainnet
-contract AgToken is IAgToken, ERC20PermitUpgradeable {
-    // ========================= References to other contracts =====================
+contract AgTokenSideChain is IAgToken, ERC20PermitUpgradeable {
+
+    // ======================= Parameters and Variables ============================
 
     /// @inheritdoc IAgToken
-    address public stableMaster;
+    mapping(address => bool) public isMinter;
+    /// @notice Reference to the treasury contract which can grant minting rights
+    ITreasury public treasury;
 
     // ============================= Constructor ===================================
 
     /// @notice Initializes the `AgToken` contract
     /// @param name_ Name of the token
     /// @param symbol_ Symbol of the token
-    /// @param stableMaster_ Reference to the `StableMaster` contract associated to this agToken
+    /// @param _treasury Reference to the `Treasury` contract associated to this agToken
     /// @dev By default, agTokens are ERC-20 tokens with 18 decimals
     function initialize(
         string memory name_,
         string memory symbol_,
-        address stableMaster_
+        ITreasury _treasury
     ) external initializer {
         __ERC20Permit_init(name_);
         __ERC20_init(name_, symbol_);
-        require(stableMaster_ != address(0), "0");
-        stableMaster = stableMaster_;
+        require(address(ITreasury(_treasury).stablecoin()) == address(this), "6");
+        treasury = ITreasury(_treasury);
     }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
-    // ======= Added Parameters and Variables from the first implementation ========
-
-    /// @inheritdoc IAgToken
-    mapping(address => bool) public isMinter;
-    /// @notice Reference to the treasury contract which can grant minting rights
-    ITreasury public treasury;
-    /// @notice Boolean to check whether the contract has been reinitialized after its upgrade
-    bool public treasuryInitialized;
-
     // =============================== Added Events ================================
 
     event TreasuryUpdated(address indexed _treasury);
     event MinterToggled(address indexed minter);
-
-    // =============================== Setup Function ==============================
-
-    /// @notice Sets up the treasury contract in this AgToken contract
-    /// @param _treasury Treasury contract to add
-    /// @dev The address calling this function has to be hard-coded in the contract
-    function setUpTreasury(address _treasury) external {
-        require(msg.sender == 0xdC4e6DFe07EFCa50a197DF15D9200883eF4Eb1c8, "1");
-        require(address(ITreasury(_treasury).stablecoin()) == address(this), "6");
-        require(!treasuryInitialized, "34");
-        treasury = ITreasury(_treasury);
-        treasuryInitialized = true;
-        isMinter[stableMaster] = true;
-        emit TreasuryUpdated(_treasury);
-    }
 
     // =============================== Modifiers ===================================
 
@@ -85,33 +64,8 @@ contract AgToken is IAgToken, ERC20PermitUpgradeable {
         _;
     }
 
-    // ========================= External Functions ================================
-    // The following functions allow anyone to burn stablecoins without redeeming collateral
-    // in exchange for that
+    // =========================== External Function ===============================
 
-    /// @notice Destroys `amount` token from the caller without giving collateral back
-    /// @param amount Amount to burn
-    /// @param poolManager Reference to the `PoolManager` contract for which the `stocksUsers` will
-    /// need to be updated
-    /// @dev When calling this function, people should specify the `poolManager` for which they want to decrease
-    /// the `stocksUsers`: this a way for the protocol to maintain healthy accounting variables
-    function burnNoRedeem(uint256 amount, address poolManager) external {
-        _burn(msg.sender, amount);
-        IStableMaster(stableMaster).updateStocksUsers(amount, poolManager);
-    }
-
-    /// @notice Burns `amount` of agToken on behalf of another account without redeeming collateral back
-    /// @param account Account to burn on behalf of
-    /// @param amount Amount to burn
-    /// @param poolManager Reference to the `PoolManager` contract for which the `stocksUsers` will need to be updated
-    function burnFromNoRedeem(
-        address account,
-        uint256 amount,
-        address poolManager
-    ) external {
-        _burnFromNoRedeem(amount, account, msg.sender);
-        IStableMaster(stableMaster).updateStocksUsers(amount, poolManager);
-    }
 
     /// @notice Allows anyone to burn agToken without redeeming collateral back
     /// @param amount Amount of stablecoins to burn
@@ -152,8 +106,7 @@ contract AgToken is IAgToken, ERC20PermitUpgradeable {
 
     /// @inheritdoc IAgToken
     function removeMinter(address minter) external {
-        // The `treasury` contract cannot remove the `stableMaster`
-        require((msg.sender == address(treasury) && minter != stableMaster) || msg.sender == minter, "36");
+        require(msg.sender == address(treasury) || msg.sender == minter, "36");
         isMinter[minter] = false;
         emit MinterToggled(minter);
     }
