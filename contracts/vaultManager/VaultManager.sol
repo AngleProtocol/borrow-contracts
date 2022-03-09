6962dfe98ce028b615d4dcd760a50d2e56694a08
Pablo Veyrat <veyratpablo@gmail.com>
2022-03-09 20:10:37 +0100

    fixing handle-repay

--- contracts/vaultManager/VaultManager.sol
+++ contracts/vaultManager/VaultManager.sol
@@ -238,6 +238,7 @@ contract VaultManager is VaultManagerERC721, IVaultManagerFunctions {
                 uint256 collateralPayment = paymentData.collateralAmountToReceive - paymentData.collateralAmountToGive;
                 if (repayData.length > 0 && collateralPayment > 0) {
                     IRepayCallee(who).repayCallCollateral(msg.sender, stablecoinPayment, collateralPayment, repayData);
+                    collateral.safeTransferFrom(msg.sender, address(this), collateralPayment);
                 } else if (collateralPayment > 0)
                     collateral.safeTransferFrom(msg.sender, address(this), collateralPayment);
             }
