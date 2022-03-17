3153d6e20f30a77aef189ce995366438a2486419
picodes <picodes@angle.money>
2022-03-17 16:47:37 +0100

    lint

--- deploy/7_vaultManagerProxy.ts
+++ deploy/7_vaultManagerProxy.ts
@@ -1,27 +1,41 @@
 import { ChainId, CONTRACTS_ADDRESSES } from '@angleprotocol/sdk/dist';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { Contract } from 'ethers';
 import hre from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import yargs from 'yargs';
 
-import { AgToken, Treasury, VaultManager__factory } from '../typechain';
+import { Treasury__factory, VaultManager__factory } from '../typechain';
 import params from './networks';
 const argv = yargs.env('').boolean('ci').parseSync();
 
 const func: DeployFunction = async ({ deployments, ethers, network }) => {
   const { deploy } = deployments;
   const { deployer } = await ethers.getNamedSigners();
+  const json = await import('./networks/' + network.name + '.json');
+  const governor = json.governor;
+
   let proxyAdminAddress: string;
   const implementation = (await ethers.getContract('VaultManager_Implementation')).address;
-  const treasury = (await ethers.getContract('Treasury')) as Treasury;
-  const agToken = (await ethers.getContract('AgToken')) as AgToken;
+  const treasuryAddress = (await ethers.getContract('Treasury')).address;
+  let signer: SignerWithAddress;
 
   if (!network.live) {
     // If we're in mainnet fork, we're using the `ProxyAdmin` address from mainnet
     proxyAdminAddress = CONTRACTS_ADDRESSES[ChainId.MAINNET].ProxyAdmin!;
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [governor],
+    });
+    await hre.network.provider.send('hardhat_setBalance', [governor, '0x10000000000000000000000000000']);
+    signer = await ethers.getSigner(governor);
   } else {
     // Otherwise, we're using the proxy admin address from the desired network
     proxyAdminAddress = CONTRACTS_ADDRESSES[network.config.chainId as ChainId].ProxyAdmin!;
+    signer = deployer;
   }
+
+  const treasury = new Contract(treasuryAddress, Treasury__factory.abi, signer);
 
   console.log('Deploying proxies for vaultManager');
 
@@ -51,10 +65,12 @@ const func: DeployFunction = async ({ deployments, ethers, network }) => {
         log: !argv.ci,
       });
       const vaultManagerAddress = (await deployments.get(name)).address;
-
-      await (await treasury.addVaultManager(vaultManagerAddress)).wait();
-
       console.log(`Successfully deployed ${name} at the address ${vaultManagerAddress}`);
+      console.log('');
+
+      console.log('Now setting the VaultManager in the treasury...');
+      await (await treasury.addVaultManager(vaultManagerAddress)).wait();
+      console.log(`Success`);
       console.log('');
     }
   }
