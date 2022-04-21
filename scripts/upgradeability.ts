561e645b7eb3e08c702f8b6c744746f5f6c4daa7
The Pseudonymous Picodes <41673773+Picodes@users.noreply.github.com>
2022-04-21 10:56:15 +0200

    upgradeability security tests example (#61)
    Co-authored-by: Pablo Veyrat <50438397+sogipec@users.noreply.github.com>

---
+++ scripts/upgradeability.ts
@@ -0,0 +1,42 @@
+import { UpgradeableContract } from '@openzeppelin/upgrades-core';
+import { artifacts } from 'hardhat';
+
+const testUpgradeability = async (name: string, file: string) => {
+  const buildInfo = await artifacts.getBuildInfo(`${file}:${name}`);
+  const baseContract = new UpgradeableContract(name, buildInfo?.input as any, buildInfo?.output as any);
+  console.log(name);
+  console.log(baseContract.getErrorReport().explain());
+  console.log('');
+};
+
+const testStorage = async (name: string, file: string, nameUpgrade: string, fileUpgrade: string) => {
+  const buildInfo = await artifacts.getBuildInfo(`${file}:${name}`);
+  const baseContract = new UpgradeableContract(name, buildInfo?.input as any, buildInfo?.output as any);
+
+  const upgradeBuildInfo = await artifacts.getBuildInfo(`${fileUpgrade}:${nameUpgrade}`);
+  const upgradeContract = new UpgradeableContract(
+    nameUpgrade,
+    upgradeBuildInfo?.input as any,
+    upgradeBuildInfo?.output as any,
+  );
+  console.log('Upgrade Testing');
+  console.log(baseContract.getStorageUpgradeReport(upgradeContract).explain());
+  console.log('');
+};
+
+async function main() {
+  // Uncomment to check all valid build names
+  // console.log((await artifacts.getAllFullyQualifiedNames()));
+
+  testUpgradeability('AgToken', 'contracts/agToken/AgToken.sol');
+  testUpgradeability('AgTokenIntermediateUpgrade', 'contracts/agToken/AgTokenIntermediateUpgrade.sol');
+  testUpgradeability('FlashAngle', 'contracts/flashloan/FlashAngle.sol');
+  testUpgradeability('EulerReactor', 'contracts/reactor/EulerReactor.sol');
+  testUpgradeability('VaultManager', 'contracts/vaultManager/VaultManager.sol');
+  testUpgradeability('Treasury', 'contracts/treasury/Treasury.sol');
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
