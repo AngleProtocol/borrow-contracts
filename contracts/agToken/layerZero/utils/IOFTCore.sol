33bb6bfa6df9a16114eb344d562ee08e80840081
picodes <picodes@angle.money>
2022-06-28 17:31:18 +0200

    Comments

--- contracts/agToken/layerZero/utils/IOFTCore.sol
+++ contracts/agToken/layerZero/utils/IOFTCore.sol
@@ -7,16 +7,15 @@ import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 /**
  * @dev Interface of the IOFT core standard
+ * @dev Forked from https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/token/oft/IOFTCore.sol
  */
 interface IOFTCore is IERC165 {
-    /**
-     * @dev estimate send token `_tokenId` to (`_dstChainId`, `_toAddress`)
-     * _dstChainId - L0 defined chain id to send tokens too
-     * _toAddress - dynamic bytes array which contains the address to whom you are sending tokens to on the dstChain
-     * _amount - amount of the tokens to transfer
-     * _useZro - indicates to use zro to pay L0 fees
-     * _adapterParam - flexible bytes array to indicate messaging adapter services in L0
-     */
+    /// @notice Estimates send token `_tokenId` to (`_dstChainId`, `_toAddress`)
+    /// @param _dstChainId L0 defined chain id to send tokens too
+    /// @param _toAddress dynamic bytes array which contains the address to whom you are sending tokens to on the dstChain
+    /// @param _amount amount of the tokens to transfer
+    /// @param _useZro indicates to use zro to pay L0 fees
+    /// @param _adapterParams flexible bytes array to indicate messaging adapter services in L0
     function estimateSendFee(
         uint16 _dstChainId,
         bytes calldata _toAddress,
@@ -25,15 +24,13 @@ interface IOFTCore is IERC165 {
         bytes calldata _adapterParams
     ) external view returns (uint256 nativeFee, uint256 zroFee);
 
-    /**
-     * @dev send `_amount` amount of token to (`_dstChainId`, `_toAddress`)
-     * `_dstChainId` the destination chain identifier
-     * `_toAddress` can be any size depending on the `dstChainId`.
-     * `_amount` the quantity of tokens in wei
-     * `_refundAddress` the address LayerZero refunds if too much message fee is sent
-     * `_zroPaymentAddress` set to address(0x0) if not paying in ZRO (LayerZero Token)
-     * `_adapterParams` is a flexible bytes array to indicate messaging adapter services
-     */
+    /// @notice Sends `_amount` amount of token to (`_dstChainId`, `_toAddress`)
+    /// @param _dstChainId the destination chain identifier
+    /// @param _toAddress can be any size depending on the `dstChainId`.
+    /// @param _amount the quantity of tokens in wei
+    /// @param _refundAddress the address LayerZero refunds if too much message fee is sent
+    /// @param _zroPaymentAddress set to address(0x0) if not paying in ZRO (LayerZero Token)
+    /// @param _adapterParams is a flexible bytes array to indicate messaging adapter services
     function send(
         uint16 _dstChainId,
         bytes calldata _toAddress,
@@ -43,10 +40,30 @@ interface IOFTCore is IERC165 {
         bytes calldata _adapterParams
     ) external payable;
 
-    /**
-     * @dev Emitted when `_amount` tokens are moved from the `_sender` to (`_dstChainId`, `_toAddress`)
-     * `_nonce` is the outbound nonce
-     */
+    /// @notice Sends `_amount` amount of token to (`_dstChainId`, `_toAddress`)
+    /// @param _dstChainId The destination chain identifier
+    /// @param _toAddress Can be any size depending on the `dstChainId`.
+    /// @param _amount Quantity of tokens in wei
+    /// @param _refundAddress Address LayerZero refunds if too much message fee is sent
+    /// @param _zroPaymentAddress Set to address(0x0) if not paying in ZRO (LayerZero Token)
+    /// @param _adapterParams Flexible bytes array to indicate messaging adapter services
+    /// @param deadline Deadline parameter for the signature to be valid
+    /// @dev The `v`, `r`, and `s` parameters are used as signature data
+    function sendWithPermit(
+        uint16 _dstChainId,
+        bytes memory _toAddress,
+        uint256 _amount,
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes memory _adapterParams,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+
+    /// @dev Emitted when `_amount` tokens are moved from the `_sender` to (`_dstChainId`, `_toAddress`)
+    /// `_nonce` is the outbound nonce
     event SendToChain(
         address indexed _sender,
         uint16 indexed _dstChainId,
@@ -55,10 +72,8 @@ interface IOFTCore is IERC165 {
         uint64 _nonce
     );
 
-    /**
-     * @dev Emitted when `_amount` tokens are received from `_srcChainId` into the `_toAddress` on the local chain.
-     * `_nonce` is the inbound nonce.
-     */
+    /// @dev Emitted when `_amount` tokens are received from `_srcChainId` into the `_toAddress` on the local chain.
+    /// `_nonce` is the inbound nonce.
     event ReceiveFromChain(
         uint16 indexed _srcChainId,
         bytes indexed _srcAddress,
@@ -68,9 +83,7 @@ interface IOFTCore is IERC165 {
     );
 }
 
-/**
- * @dev Interface of the OFT standard
- */
+/// @dev Interface of the OFT standard
 interface IOFT is IOFTCore, IERC20 {
 
 }
--- contracts/agToken/layerZero/utils/OFTCore.sol
+++ contracts/agToken/layerZero/utils/OFTCore.sol
@@ -6,31 +6,27 @@ import "./NonblockingLzApp.sol";
 import "./IOFTCore.sol";
 import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
 
+/// @title OFTCore
+/// @author Angle Core Team, forked from https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/token/oft/OFTCore.sol
+/// @notice Base contract for for bridging using LayerZero
 abstract contract OFTCore is NonblockingLzApp, ERC165Upgradeable, IOFTCore {
-    uint256[50] private __gap;
+    // ==================== External Permissionless Functions ======================
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC165Upgradeable, IERC165)
-        returns (bool)
-    {
-        return interfaceId == type(IOFTCore).interfaceId || super.supportsInterface(interfaceId);
-    }
-
-    function estimateSendFee(
+    /// @inheritdoc IOFTCore
+    function sendWithPermit(
         uint16 _dstChainId,
         bytes memory _toAddress,
         uint256 _amount,
-        bool _useZro,
-        bytes memory _adapterParams
-    ) public view virtual override returns (uint256 nativeFee, uint256 zroFee) {
-        // mock the payload for send()
-        bytes memory payload = abi.encode(_toAddress, _amount);
-        return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, _adapterParams);
-    }
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes memory _adapterParams,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public payable virtual {}
 
+    /// @inheritdoc IOFTCore
     function send(
         uint16 _dstChainId,
         bytes memory _toAddress,
@@ -39,9 +35,18 @@ abstract contract OFTCore is NonblockingLzApp, ERC165Upgradeable, IOFTCore {
         address _zroPaymentAddress,
         bytes memory _adapterParams
     ) public payable virtual {
-        _send(_dstChainId, _toAddress, _amount, _refundAddress, _zroPaymentAddress, _adapterParams);
+        _amount = _debitFrom(_dstChainId, _toAddress, _amount);
+
+        bytes memory payload = abi.encode(_toAddress, _amount);
+        _lzSend(_dstChainId, payload, _refundAddress, _zroPaymentAddress, _adapterParams);
+
+        uint64 nonce = lzEndpoint.getOutboundNonce(_dstChainId, address(this));
+        emit SendToChain(msg.sender, _dstChainId, _toAddress, _amount, nonce);
     }
 
+    // ============================= Internal Functions ===================================
+
+    /// @inheritdoc NonblockingLzApp
     function _nonblockingLzReceive(
         uint16 _srcChainId,
         bytes memory _srcAddress,
@@ -60,32 +65,51 @@ abstract contract OFTCore is NonblockingLzApp, ERC165Upgradeable, IOFTCore {
         emit ReceiveFromChain(_srcChainId, _srcAddress, toAddress, amount, _nonce);
     }
 
-    function _send(
-        uint16 _dstChainId,
-        bytes memory _toAddress,
-        uint256 _amount,
-        address payable _refundAddress,
-        address _zroPaymentAddress,
-        bytes memory _adapterParams
-    ) internal virtual {
-        _amount = _debitFrom(_dstChainId, _toAddress, _amount);
-
-        bytes memory payload = abi.encode(_toAddress, _amount);
-        _lzSend(_dstChainId, payload, _refundAddress, _zroPaymentAddress, _adapterParams);
-
-        uint64 nonce = lzEndpoint.getOutboundNonce(_dstChainId, address(this));
-        emit SendToChain(msg.sender, _dstChainId, _toAddress, _amount, nonce);
-    }
-
+    /// @notice Makes accountability when bridging from this contract
+    /// @param _dstChainId ChainId of the destination chain - LayerZero standard
+    /// @param _toAddress Recipient on the destination chain
+    /// @param _amount Amount to bridge
     function _debitFrom(
         uint16 _dstChainId,
         bytes memory _toAddress,
         uint256 _amount
     ) internal virtual returns (uint256);
 
+    /// @notice Makes accountability when bridging to this contract
+    /// @param _srcChainId ChainId of the source chain - LayerZero standard
+    /// @param _toAddress Recipient on this chain
+    /// @param _amount Amount to bridge
     function _creditTo(
         uint16 _srcChainId,
         address _toAddress,
         uint256 _amount
     ) internal virtual returns (uint256);
+
+    // ======================= View Functions ================================
+
+    /// @inheritdoc ERC165Upgradeable
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165Upgradeable, IERC165)
+        returns (bool)
+    {
+        return interfaceId == type(IOFTCore).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /// @inheritdoc IOFTCore
+    function estimateSendFee(
+        uint16 _dstChainId,
+        bytes memory _toAddress,
+        uint256 _amount,
+        bool _useZro,
+        bytes memory _adapterParams
+    ) public view virtual override returns (uint256 nativeFee, uint256 zroFee) {
+        // mock the payload for send()
+        bytes memory payload = abi.encode(_toAddress, _amount);
+        return lzEndpoint.estimateFees(_dstChainId, address(this), payload, _useZro, _adapterParams);
+    }
+
+    uint256[50] private __gap;
 }
