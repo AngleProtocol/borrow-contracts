bbaac5a433257796eb6676c6d58481da909f4ef9
Pablo Veyrat <veyratpablo@gmail.com>
2022-07-11 17:22:34 +0200

    deploying multicall with failure contract

--- deploy/bridges/mockLayerZeroBridge.ts
+++ deploy/bridges/mockLayerZeroBridge.ts
@@ -14,6 +14,7 @@ import LZ_ENDPOINTS from '../constants/layerzeroEndpoints.json';
 import { deploy, deployImplem, deployProxy } from '../helpers';
 
 const func: DeployFunction = async ({ ethers, network }) => {
+  /*
   // Using an EOA as proxyAdmin as it's a mock deployment
   const { deployer, proxyAdmin } = await ethers.getNamedSigners();
 
@@ -74,6 +75,7 @@ const func: DeployFunction = async ({ ethers, network }) => {
       false,
     )
   ).wait();
+  */
 };
 
 func.tags = ['mockLayerZeroBridge'];
