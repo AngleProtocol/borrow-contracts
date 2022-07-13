61bcc7339c6d50b49890e0b31f25dc3933defb93
Pablo Veyrat <50438397+sogipec@users.noreply.github.com>
2022-07-13 11:40:48 +0200

    Keeeper multicall deploy (#103)
    * preparing multicall deployment
    
    * deploying multicall with failure contract

--- deploy/multicallWithFailure.ts
+++ deploy/multicallWithFailure.ts
@@ -1,6 +1,6 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 
-const func: DeployFunction = async ({ deployments, ethers }) => {
+const func: DeployFunction = async ({ deployments, ethers, network }) => {
   const { deploy } = deployments;
   const { deployer } = await ethers.getNamedSigners();
 
@@ -11,6 +11,7 @@ const func: DeployFunction = async ({ deployments, ethers }) => {
   });
 
   console.log(`Successfully deployed MulticallWithFailure at the address ${multicallWithFailure.address}\n`);
+  console.log(`yarn hardhat verify --network ${network.name} ${multicallWithFailure.address}`);
 };
 
 func.tags = ['multicall_with_failure'];
