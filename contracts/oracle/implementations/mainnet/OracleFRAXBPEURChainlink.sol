c34c9ccc8d94f09d9fa8c3ef35686e2e33ad1f7a
Pablo Veyrat <veyratpablo@gmail.com>
2022-11-01 10:00:21 +0100

    fixing with respect to guillaume comments

--- contracts/oracle/implementations/mainnet/OracleFRAXBPEURChainlink.sol
+++ contracts/oracle/implementations/mainnet/OracleFRAXBPEURChainlink.sol
@@ -21,20 +21,25 @@ contract OracleFRAXBPEURChainlink is BaseOracleChainlinkMulti {
 
     /// @inheritdoc IOracle
     function read() external view override returns (uint256 quoteAmount) {
-        uint256 fraxPrice = _readOracle(AggregatorV3Interface(0xB9E1E3A9feFf48998E45Fa90847ed4D467E8BcfD));
-        uint256 usdcPrice = _readOracle(AggregatorV3Interface(0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6));
-        uint256 eurPrice = _readOracle(AggregatorV3Interface(0xb49f677943BC038e9857d61E7d053CaA2C1734C1));
+        uint256 fraxPrice = _readChainlinkFeed(
+            1,
+            AggregatorV3Interface(0xB9E1E3A9feFf48998E45Fa90847ed4D467E8BcfD),
+            1,
+            0
+        );
+        uint256 usdcPrice = _readChainlinkFeed(
+            1,
+            AggregatorV3Interface(0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6),
+            1,
+            0
+        );
         // Picking the minimum price between FRAX and USDC, multiplying it by the pool's virtual price
         usdcPrice = usdcPrice >= fraxPrice ? fraxPrice : usdcPrice;
-        quoteAmount = FRAXBP.get_virtual_price();
-        quoteAmount = (quoteAmount * usdcPrice) / eurPrice;
-    }
-
-    /// @notice Helper to read the value provided by `feed`
-    function _readOracle(AggregatorV3Interface feed) internal view returns (uint256) {
-        (uint80 roundId, int256 ratio, , uint256 updatedAt, uint80 answeredInRound) = feed.latestRoundData();
-        if (ratio <= 0 || roundId > answeredInRound || block.timestamp - updatedAt > stalePeriod)
-            revert InvalidChainlinkRate();
-        return (uint256(ratio));
+        quoteAmount = _readChainlinkFeed(
+            (FRAXBP.get_virtual_price() * usdcPrice),
+            AggregatorV3Interface(0xb49f677943BC038e9857d61E7d053CaA2C1734C1),
+            0,
+            0
+        );
     }
 }
