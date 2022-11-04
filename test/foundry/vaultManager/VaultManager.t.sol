78d9b5fbcfa46866743e05d53598056e9ec7b70b
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2022-11-04 18:24:40 +0100

    fuzzing test on vaultManagerListing

--- test/foundry/vaultManager/VaultManager.t.sol
+++ test/foundry/vaultManager/VaultManager.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.12;
 
 import { console } from "forge-std/console.sol";
 import { stdStorage, StdStorage, Test } from "forge-std/Test.sol";
-import "../../../contracts/vaultManager/VaultManager.sol";
+import { VaultManager, VaultManagerStorage } from "../../../contracts/vaultManager/VaultManager.sol";
 import { ActionType } from "../../../contracts/interfaces/IVaultManager.sol";
 import "../../../contracts/treasury/Treasury.sol";
 import "../../../contracts/mock/MockStableMaster.sol";
