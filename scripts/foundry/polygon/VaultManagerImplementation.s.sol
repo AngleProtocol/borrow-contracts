43f83a586ab2fd4d5e96f4ba72f6658674add007
Pablo Veyrat <veyratpablo@gmail.com>
2022-11-15 12:54:55 +0100

    fixing foundry tests

---
+++ scripts/foundry/polygon/VaultManagerImplementation.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+import "forge-std/Script.sol";
+import { VaultManagerListing } from "../../../contracts/vaultManager/VaultManagerListing.sol";
+import "./PolygonConstants.s.sol";
+
+contract DeployVaultManagerImplementation is Script, PolygonConstants {
+    VaultManagerListing public vaultManagerImplementation;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC_POLYGON"), 2);
+        vm.startBroadcast(deployerPrivateKey);
+
+        vaultManagerImplementation = new VaultManagerListing();
+
+        console.log(
+            "Successfully deployed vaultManagerImplementation at the address: ",
+            address(vaultManagerImplementation)
+        );
+
+        vm.stopBroadcast();
+    }
+}
