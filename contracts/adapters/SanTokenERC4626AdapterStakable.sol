73163e44acf00571be8ce9978ad6982ef66c3d57
Pablo Veyrat <veyratpablo@gmail.com>
2022-11-28 14:38:55 +0100

    sanToken adapters

--- contracts/adapters/SanTokenERC4626AdapterStakable.sol
+++ contracts/adapters/SanTokenERC4626AdapterStakable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.12;
+pragma solidity ^0.8.12;
 
 import "./SanTokenERC4626Adapter.sol";
 
@@ -58,10 +58,9 @@ contract SanTokenERC4626AdapterStakable is SanTokenERC4626Adapter {
     ) internal override {
         // Not claiming only if it is a deposit
         bool _claim = !(_from == address(0));
-        address[] memory checkpointUser = new address[](2);
-        checkpointUser[0] = _from;
-        checkpointUser[1] = _to;
-        _checkpoint(checkpointUser, _claim);
+        _claimRewards();
+        _checkpointRewardsUser(_from, _claim);
+        _checkpointRewardsUser(_to, _claim);
         // If the user is withdrawing, we need to unstake from the gauge
         if (_to == address(0)) gauge.withdraw(amount, false);
         if (_from == address(0)) gauge.deposit(amount, address(this), false);
@@ -74,9 +73,8 @@ contract SanTokenERC4626AdapterStakable is SanTokenERC4626Adapter {
     /// @return rewardAmounts Amounts of each reward token claimed by the user
     //solhint-disable-next-line
     function claim_rewards(address from) external returns (uint256[] memory) {
-        address[] memory checkpointUser = new address[](1);
-        checkpointUser[0] = address(from);
-        return _checkpoint(checkpointUser, true);
+        _claimRewards();
+        return _checkpointRewardsUser(from, true);
     }
 
     /// @notice Returns the exact amount that will be received if calling `claim_rewards(from)` for a specific reward token
@@ -93,21 +91,6 @@ contract SanTokenERC4626AdapterStakable is SanTokenERC4626Adapter {
 
     // ======================== INTERNAL ACCOUNTING HELPERS ========================
 
-    /// @notice Claims contracts rewards and checkpoints rewards for different `accounts`
-    /// @param accounts Array of accounts we should checkpoint rewards for
-    /// @param _claim Whether to claim for `accounts` the pending rewards
-    /// @return rewardAmounts An array representing the rewards earned by the first address in the `accounts` array
-    /// on each of the reward token
-    function _checkpoint(address[] memory accounts, bool _claim) internal returns (uint256[] memory rewardAmounts) {
-        _claimRewards();
-        uint256 accountsLength = accounts.length;
-        for (uint256 i; i < accountsLength; ++i) {
-            if (accounts[i] == address(0)) continue;
-            if (i == 0) rewardAmounts = _checkpointRewardsUser(accounts[i], _claim);
-            else _checkpointRewardsUser(accounts[i], _claim);
-        }
-    }
-
     /// @notice Checkpoints rewards earned by a user
     /// @param from Address to claim rewards from
     /// @param _claim Whether to claim or not the rewards
@@ -116,28 +99,23 @@ contract SanTokenERC4626AdapterStakable is SanTokenERC4626Adapter {
         IERC20[] memory rewardTokens = _getRewards();
         uint256 rewardTokensLength = rewardTokens.length;
         rewardAmounts = new uint256[](rewardTokensLength);
+        if (from == address(0)) return rewardAmounts;
         uint256 userBalance = balanceOf(from);
         for (uint256 i; i < rewardTokensLength; ++i) {
-            uint256 newClaimable = (userBalance * (integral[rewardTokens[i]] - integralOf[rewardTokens[i]][from])) /
-                _BASE_PARAMS;
-            uint256 previousClaimable = pendingRewardsOf[rewardTokens[i]][from];
-            if (_claim && previousClaimable + newClaimable != 0) {
-                pendingRewardsOf[rewardTokens[i]][from] = 0;
-                rewardTokens[i].safeTransfer(from, previousClaimable + newClaimable);
-            } else if (newClaimable != 0) {
-                pendingRewardsOf[rewardTokens[i]][from] += newClaimable;
+            uint256 totalClaimable = (userBalance * (integral[rewardTokens[i]] - integralOf[rewardTokens[i]][from])) /
+                _BASE_PARAMS +
+                pendingRewardsOf[rewardTokens[i]][from];
+            if (totalClaimable != 0) {
+                if (_claim) {
+                    pendingRewardsOf[rewardTokens[i]][from] = 0;
+                    rewardTokens[i].safeTransfer(from, totalClaimable);
+                } else {
+                    pendingRewardsOf[rewardTokens[i]][from] = totalClaimable;
+                }
+                rewardAmounts[i] = totalClaimable;
             }
             integralOf[rewardTokens[i]][from] = integral[rewardTokens[i]];
-            rewardAmounts[i] = previousClaimable + newClaimable;
         }
-    }
-
-    /// @notice Adds the contract claimed rewards to the distributed rewards
-    /// @param rewardToken Reward token that must be updated
-    /// @param amount Amount to add to the claimable rewards
-    function _updateRewards(IERC20 rewardToken, uint256 amount) internal {
-        uint256 _totalSupply = totalSupply();
-        if (_totalSupply != 0) integral[rewardToken] += (amount * _BASE_PARAMS) / _totalSupply;
     }
 
     /// @notice Claims all available rewards and increases the associated integral
@@ -147,6 +125,14 @@ contract SanTokenERC4626AdapterStakable is SanTokenERC4626Adapter {
         uint256 angleRewards = _ANGLE.balanceOf(address(this)) - prevBalanceAngle;
         // Do the same thing for additional rewards
         _updateRewards(_ANGLE, angleRewards);
+    }
+
+    /// @notice Adds the contract claimed rewards to the distributed rewards
+    /// @param rewardToken Reward token that must be updated
+    /// @param amount Amount to add to the claimable rewards
+    function _updateRewards(IERC20 rewardToken, uint256 amount) internal {
+        uint256 _totalSupply = totalSupply();
+        if (_totalSupply != 0) integral[rewardToken] += (amount * _BASE_PARAMS) / _totalSupply;
     }
 
     /// @notice Gets the reward tokens given in the liquidity gauge
--- test/hardhat/adapters/SanTokenERC4626Adapter.test.ts
+++ test/hardhat/adapters/SanTokenERC4626Adapter.test.ts
@@ -1,4 +1,4 @@
-import { ChainId, registry, StableMasterFront__factory } from '@angleprotocol/sdk';
+import { ChainId, FeeManager__factory, registry, StableMasterFront__factory } from '@angleprotocol/sdk';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { BigNumber, Signer } from 'ethers';
 import { parseEther, parseUnits } from 'ethers/lib/utils';
@@ -21,13 +21,13 @@ contract('SanTokenERC4626Adapter', () => {
   let adapter: SanTokenERC4626Adapter;
   let usdc: MockTokenPermit;
   let stableMaster: string;
+  let feeManager: string;
   let poolManager: string;
   let sanRateAtBlock: string;
-  let gauge: string;
   let assets: string;
-  let sanHolder: string;
   let sanToken: MockTokenPermit;
   let usdcHolder: string;
+  let governor: string;
 
   const impersonatedSigners: { [key: string]: Signer } = {};
 
@@ -44,11 +44,10 @@ contract('SanTokenERC4626Adapter', () => {
       '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     )) as MockTokenPermit;
     sanRateAtBlock = '1143918107079882679';
-    // gauge address has more in value than what's needed
-    gauge = '0x51fE22abAF4a26631b2913E417c0560D547797a7';
     assets = '2101885141874';
-    sanHolder = '0x9C215206Da4bf108aE5aEEf9dA7caD3352A36Dad';
     usdcHolder = '0x4943b0c9959dcf58871a799dfb71bece0d97c9f4';
+    feeManager = '0x97B6897AAd7aBa3861c04C0e6388Fc02AF1F227f';
+    governor = '0xdC4e6DFe07EFCa50a197DF15D9200883eF4Eb1c8';
   });
 
   beforeEach(async () => {
@@ -67,7 +66,7 @@ contract('SanTokenERC4626Adapter', () => {
     adapter = (await deployUpgradeable(new SanTokenERC4626Adapter__factory(deployer))) as SanTokenERC4626Adapter;
     await adapter.initialize(stableMaster, poolManager);
 
-    const impersonatedAddresses = [usdcHolder];
+    const impersonatedAddresses = [usdcHolder, governor];
     for (const address of impersonatedAddresses) {
       await hre.network.provider.request({
         method: 'hardhat_impersonateAccount',
@@ -79,6 +78,7 @@ contract('SanTokenERC4626Adapter', () => {
   });
   describe('initializer', () => {
     it('success - stableMaster, name, symbol', async () => {
+      await expect(adapter.initialize(stableMaster, poolManager)).to.be.reverted;
       expect(await adapter.decimals()).to.be.equal(6);
       expect(await adapter.name()).to.be.equal('Angle sanUSDC_EUR Wrapper');
       expect(await adapter.symbol()).to.be.equal('ag-wrapper-sanUSDC_EUR');
@@ -209,6 +209,54 @@ contract('SanTokenERC4626Adapter', () => {
       await adapter.connect(alice).redeem(parseUnits('1', 6), bob.address, alice.address);
       expectApprox(await usdc.balanceOf(bob.address), amount, 0.1);
       expect(await sanToken.balanceOf(adapter.address)).to.be.equal(0);
+    });
+  });
+  describe('slippage', () => {
+    it('success - withdrawal values are now smaller', async () => {
+      const feeManagerContract = new ethers.Contract(feeManager, FeeManager__factory.createInterface(), alice);
+      await feeManagerContract.connect(impersonatedSigners[governor]).setFees([0], [parseUnits('0.2', 9)], 3);
+      await feeManagerContract.connect(impersonatedSigners[governor]).updateUsersSLP();
+      // Slippage should be 20%
+      expect(await adapter.previewWithdraw(parseEther('1'))).to.be.equal(MAX_UINT256);
+      expect(await adapter.previewWithdraw(parseUnits('1', 6))).to.be.equal(
+        parseUnits('1.25', 6).mul(parseEther('1')).div(sanRateAtBlock).add(1),
+      );
+      expect(await adapter.previewRedeem(parseUnits('1', 6))).to.be.equal(
+        BigNumber.from(sanRateAtBlock).div(parseUnits('1.25', 12)),
+      );
+      expect(await adapter.previewRedeem(parseEther('100000000000000'))).to.be.equal(0);
+
+      await usdc.connect(impersonatedSigners[usdcHolder]).approve(adapter.address, MAX_UINT256);
+      await adapter.connect(impersonatedSigners[usdcHolder]).mint(parseUnits('1', 6), alice.address);
+      const amount = BigNumber.from(sanRateAtBlock).div(parseUnits('1', 12));
+      expect(await sanToken.balanceOf(adapter.address)).to.be.equal(parseUnits('1', 6));
+      expect(await adapter.balanceOf(alice.address)).to.be.equal(parseUnits('1', 6));
+      // A bit more assets are used in the process
+      expect(await adapter.totalAssets()).to.be.equal(amount.add(assets).add(1));
+      // Now has 1 USDC worth of sanToken
+      expectApprox(
+        await adapter.maxWithdraw(alice.address),
+        BigNumber.from(sanRateAtBlock).div(parseUnits('1.25', 12)),
+        0.1,
+      );
+      expect(await adapter.maxRedeem(alice.address)).to.be.equal(parseUnits('1', 6));
+      // Redeeming half of the shares
+      await adapter.connect(alice).redeem(parseUnits('0.5', 6), bob.address, alice.address);
+      const balance = await usdc.balanceOf(bob.address);
+      expectApprox(balance, BigNumber.from(sanRateAtBlock).div(parseUnits('2.5', 12)), 0.1);
+      expect(await adapter.balanceOf(alice.address)).to.be.equal(parseUnits('0.5', 6));
+      expect(await sanToken.balanceOf(adapter.address)).to.be.equal(parseUnits('0.5', 6));
+
+      // Now testing a withdrawal
+      await expect(adapter.connect(alice).withdraw(parseUnits('0.5', 6), bob.address, alice.address)).to.be.reverted;
+      await adapter.connect(alice).withdraw(parseUnits('0.2', 6), bob.address, alice.address);
+      expect(await usdc.balanceOf(bob.address)).to.be.equal(balance.add(parseUnits('0.2', 6)));
+      // 0.2*1.25=0.25
+      expectApprox(
+        await adapter.balanceOf(alice.address),
+        parseUnits('0.5', 6).sub(parseUnits('0.25', 6).mul(parseEther('1')).div(sanRateAtBlock)),
+        0.1,
+      );
     });
   });
 });
