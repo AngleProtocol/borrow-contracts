9cbc1d2679c449b8b9cd2cd010bcb7263ec62098
Pablo Veyrat <veyratpablo@gmail.com>
2022-12-12 16:51:38 +0100

    changing oracle contracts to make it more modular

--- contracts/oracle/implementations/mainnet/OracleWSTETHEURChainlink.sol
+++ contracts/oracle/implementations/mainnet/OracleWSTETHEURChainlink.sol
@@ -4,39 +4,30 @@ pragma solidity ^0.8.12;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-import "../../BaseOracleChainlinkMulti.sol";
+import "../../BaseOracleChainlinkMultiTwoFeeds.sol";
 import "../../../interfaces/external/lido/IStETH.sol";
 
 /// @title OracleWSTETHEURChainlink
 /// @author Angle Labs, Inc.
 /// @notice Gives the price of wSTETH in Euro in base 18
-contract OracleWSTETHEURChainlink is BaseOracleChainlinkMulti {
+contract OracleWSTETHEURChainlink is BaseOracleChainlinkMultiTwoFeeds {
     string public constant DESCRIPTION = "wSTETH/EUR Oracle";
     IStETH public constant STETH = IStETH(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
 
-    /// @notice Constructor of the contract
-    /// @param _stalePeriod Minimum feed update frequency for the oracle to not revert
-    /// @param _treasury Treasury associated to the `VaultManager` which reads from this feed
-    constructor(uint32 _stalePeriod, address _treasury) BaseOracleChainlinkMulti(_stalePeriod, _treasury) {}
+    constructor(uint32 _stalePeriod, address _treasury) BaseOracleChainlinkMultiTwoFeeds(_stalePeriod, _treasury) {}
 
-    /// @inheritdoc IOracle
-    function read() external view override returns (uint256 quoteAmount) {
-        quoteAmount = STETH.getPooledEthByShares(1 ether);
-        AggregatorV3Interface[2] memory circuitChainlink = [
+    /// @inheritdoc BaseOracleChainlinkMultiTwoFeeds
+    function circuitChainlink() public pure override returns (AggregatorV3Interface[2] memory) {
+        return [
             // Chainlink stETH/USD address
             AggregatorV3Interface(0xCfE54B5cD566aB89272946F602D76Ea879CAb4a8),
             // Chainlink EUR/USD address
             AggregatorV3Interface(0xb49f677943BC038e9857d61E7d053CaA2C1734C1)
         ];
-        uint8[2] memory circuitChainIsMultiplied = [1, 0];
-        uint8[2] memory chainlinkDecimals = [8, 8];
-        for (uint256 i; i < 2; ++i) {
-            quoteAmount = _readChainlinkFeed(
-                quoteAmount,
-                circuitChainlink[i],
-                circuitChainIsMultiplied[i],
-                chainlinkDecimals[i]
-            );
-        }
+    }
+
+    /// @inheritdoc BaseOracleChainlinkMultiTwoFeeds
+    function _getQuoteAmount() internal view override returns (uint256) {
+        return STETH.getPooledEthByShares(1 ether);
     }
 }
--- contracts/oracle/implementations/polygon/OracleBTCEURChainlinkPolygon.sol
+++ contracts/oracle/implementations/polygon/OracleBTCEURChainlinkPolygon.sol
@@ -4,39 +4,24 @@ pragma solidity ^0.8.12;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-import "../../BaseOracleChainlinkMulti.sol";
+import "../../BaseOracleChainlinkMultiTwoFeeds.sol";
 
 /// @title OracleBTCEURChainlinkPolygon
 /// @author Angle Labs, Inc.
 /// @notice Gives the price of BTC in Euro in base 18
 /// @dev This contract is built to be deployed on Polygon
-contract OracleBTCEURChainlinkPolygon is BaseOracleChainlinkMulti {
-    uint256 public constant OUTBASE = 10**18;
+contract OracleBTCEURChainlinkPolygon is BaseOracleChainlinkMultiTwoFeeds {
     string public constant DESCRIPTION = "BTC/EUR Oracle";
 
-    /// @notice Constructor of the contract
-    /// @param _stalePeriod Minimum feed update frequency for the oracle to not revert
-    /// @param _treasury Treasury associated to the `VaultManager` which reads from this feed
-    constructor(uint32 _stalePeriod, address _treasury) BaseOracleChainlinkMulti(_stalePeriod, _treasury) {}
+    constructor(uint32 _stalePeriod, address _treasury) BaseOracleChainlinkMultiTwoFeeds(_stalePeriod, _treasury) {}
 
-    /// @inheritdoc IOracle
-    function read() external view override returns (uint256 quoteAmount) {
-        quoteAmount = OUTBASE;
-        AggregatorV3Interface[2] memory circuitChainlink = [
-            // Oracle BTC/USD
-            AggregatorV3Interface(0xc907E116054Ad103354f2D350FD2514433D57F6f),
+    /// @inheritdoc BaseOracleChainlinkMultiTwoFeeds
+    function circuitChainlink() public pure override returns (AggregatorV3Interface[2] memory) {
+        return [
+            // Oracle ETH/USD
+            AggregatorV3Interface(0xF9680D99D6C9589e2a93a78A04A279e509205945),
             // Oracle EUR/USD
             AggregatorV3Interface(0x73366Fe0AA0Ded304479862808e02506FE556a98)
         ];
-        uint8[2] memory circuitChainIsMultiplied = [1, 0];
-        uint8[2] memory chainlinkDecimals = [8, 8];
-        for (uint256 i; i < 2; ++i) {
-            quoteAmount = _readChainlinkFeed(
-                quoteAmount,
-                circuitChainlink[i],
-                circuitChainIsMultiplied[i],
-                chainlinkDecimals[i]
-            );
-        }
     }
 }
