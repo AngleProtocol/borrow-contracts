b87f15ec9e0389c4b7722753cfa09d07b15b23c0
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2022-12-21 22:51:40 +0100

    reduce range sanRate

--- test/foundry/adapters/SanTokenERC4626AdapterTest.test.sol
+++ test/foundry/adapters/SanTokenERC4626AdapterTest.test.sol
@@ -230,7 +230,7 @@ contract SanTokenERC4626AdapterTest is BaseTest {
                 maxInterestDistributed,
                 uint64(bound(lockedInterestsSlippage[i * 2 + 1], 0, BASE_PARAMS))
             );
-            stableMaster.setSanRate(address(stableMaster), bound(sanRates[i], 10**12, 10**21));
+            stableMaster.setSanRate(address(stableMaster), bound(sanRates[i], 10**15, 10**20));
 
             if (sanTokenAdapter.balanceOf(account) == 0) isDepositWithdraw[i] = true;
             (uint256 newSanRate, uint64 slippage) = stableMaster.estimateSanRate(address(stableMaster));
@@ -317,7 +317,7 @@ contract SanTokenERC4626AdapterTest is BaseTest {
                 // to not have an arithmetic overflow in the `_sanTokenValueAfterWithdraw`
                 uint64(bound(lockedInterestsSlippage[i * 2 + 1], 0, BASE_PARAMS - 1))
             );
-            stableMaster.setSanRate(address(stableMaster), bound(sanRates[i], 10**12, 10**20));
+            stableMaster.setSanRate(address(stableMaster), bound(sanRates[i], 10**15, 10**20));
 
             if (sanTokenAdapter.balanceOf(account) == 0) isMintWithdraw[i] = true;
             uint64 slippage;
