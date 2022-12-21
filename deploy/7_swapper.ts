2291ef180327150239f4fa494ce59a91c0ff9139
Pablo Veyrat <veyratpablo@gmail.com>
2022-12-21 19:24:02 +0100

    deploying multicall with failure and swapper V1

--- deploy/7_swapper.ts
+++ deploy/7_swapper.ts
@@ -13,18 +13,20 @@ const func: DeployFunction = async ({ deployments, ethers, network }) => {
 
   console.log('Now deploying the swapper contract');
   // const angleRouter = registry(network.config.chainId as ChainId)?.AngleRouterV2;
+  // const oneInchRouter = json.oneInchRouter;
+  const oneInchRouter = '0x1111111254fb6c44bAC0beD2854e76F90643097d';
   const angleRouter = '0xf530b844fb797D2C6863D56a94777C3e411CEc86';
   console.log('Checking contract addresses');
-  console.log(`${core} ${json.uniswapV3Router} ${json.oneInchRouter} ${angleRouter}`);
+  console.log(`${core} ${json.uniswapV3Router} ${oneInchRouter} ${angleRouter}`);
 
-  await deploy(`SwapperV2`, {
+  await deploy(`Swapper`, {
     contract: 'Swapper',
     from: deployer.address,
-    args: [core, json.uniswapV3Router, json.oneInchRouter, angleRouter],
+    args: [core, json.uniswapV3Router, oneInchRouter, angleRouter],
     log: !argv.ci,
   });
   console.log('Success');
-  const swapperAddress = (await deployments.get('SwapperV2')).address;
+  const swapperAddress = (await deployments.get('Swapper')).address;
   console.log(`${swapperAddress}`);
 };
 
