4dc3f1ec75699cbf6cf423bbb779ea75f8c3265a
Pablo Veyrat <veyratpablo@gmail.com>
2023-01-18 10:42:30 +0100

    changing constants file

--- contracts/utils/Constants.sol
+++ contracts/utils/Constants.sol
@@ -8,8 +8,8 @@ error InsufficientAssets();
 /// @author Angle Labs, Inc.
 /// @notice Constants and errors for Angle Protocol contracts
 contract Constants {
-    uint256 internal constant _BASE_9 = 10**9;
-    uint256 internal constant _BASE_18 = 10**18;
-    uint256 internal constant _BASE_27 = 10**27;
-    uint256 internal constant _BASE_36 = 10**36;
+    uint256 internal constant _BASE_9 = 1e9;
+    uint256 internal constant _BASE_18 = 1e18;
+    uint256 internal constant _BASE_27 = 1e27;
+    uint256 internal constant _BASE_36 = 1e36;
 }
