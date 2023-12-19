009824a9deb3b8a6ca344cbc64cab5b857f91a48
Pablo Veyrat <veyratpablo@gmail.com>
2023-12-19 17:41:43 +0100

    feat: agToken update

--- contracts/agToken/AgTokenSideChainMultiBridge.sol
+++ contracts/agToken/AgTokenSideChainMultiBridge.sol
@@ -11,9 +11,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 /// @notice Contract for Angle agTokens on other chains than Ethereum mainnet
 /// @dev This contract supports bridge tokens having a minting right on the stablecoin (also referred to as the canonical
 /// or the native token)
-/// @dev References:
-///      - FRAX implementation: https://polygonscan.com/address/0x45c32fA6DF82ead1e2EF74d17b76547EDdFaFF89#code
-///      - QiDAO implementation: https://snowtrace.io/address/0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b#code
 contract AgTokenSideChainMultiBridge is BaseAgToken {
     using SafeERC20 for IERC20;
 
