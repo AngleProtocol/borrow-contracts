351f54804a3cc8535e9775c8a15c243fa473abca
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2024-04-26 18:02:05 +0200

    feat: PTUSDe oracle

---
+++ scripts/foundry/mainnet/PTUSDeOracle.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { MorphoFeedPTUSDe } from "borrow-contracts/oracle/morpho/mainnet/MorphoFeedPTUSDe.sol";
+import "utils/src/CommonUtils.sol";
+import { IAccessControlManager } from "borrow-contracts/interfaces/IAccessControlManager.sol";
+
+contract PTweETHOracleDeploy is Script, CommonUtils {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        vm.startBroadcast(deployerPrivateKey);
+
+        // TODO
+        uint256 chainId = CHAIN_ETHEREUM;
+        address coreBorrow = 0x5bc6BEf80DA563EBf6Df6D6913513fa9A7ec89BE;
+        uint32 _TWAP_DURATION = 30 minutes;
+        uint256 _MAX_IMPLIED_RATE = 0.5 ether;
+        // end TODO
+
+        MorphoFeedPTUSDe oracle = new MorphoFeedPTUSDe(
+            IAccessControlManager(address(coreBorrow)),
+            _MAX_IMPLIED_RATE,
+            _TWAP_DURATION
+        );
+        (, int256 answer, , , ) = oracle.latestRoundData();
+        console.log("oracle value ", uint256(answer));
+        console.log("Successfully deployed PT-weETH: ", address(oracle));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/foundry/oracles/pendle/MorphoFeedPTUSDe.t.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "./MorphoFeedPTPendle.t.sol";
+import { MorphoFeedPTUSDe } from "borrow-contracts/oracle/morpho/mainnet/MorphoFeedPTUSDe.sol";
+import { IAccessControlManager } from "borrow-contracts/interfaces/IAccessControlManager.sol";
+
+contract MorphoFeedPTUSDeTest is MorphoFeedPTPendleTest {
+    using stdStorage for StdStorage;
+
+    function setUp() public override {
+        super.setUp();
+
+        _TWAP_DURATION = 30 minutes;
+        _STALE_PERIOD = 24 hours;
+        _MAX_IMPLIED_RATE = 0.5 ether;
+
+        _oracle = BaseFeedPTPendle(
+            address(new MorphoFeedPTUSDe(IAccessControlManager(address(coreBorrow)), _MAX_IMPLIED_RATE, _TWAP_DURATION))
+        );
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                      CORE LOGIC                                                    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+    function test_Description_Success() public {
+        assertEq(_oracle.description(), "PT-USDe/USDe Oracle");
+    }
+
+    function test_Simple_Success() public {
+        (, int256 answer, , , ) = _oracle.latestRoundData();
+        uint256 value = uint256(answer);
+
+        assertApproxEqAbs(value, 0.90 ether, 0.01 ether);
+    }
+
+    function test_EconomicalLowerBound_tooSmall() public {
+        vm.prank(_governor);
+        _oracle.setMaxImpliedRate(uint256(1e1));
+        uint256 pendleAMMPrice = PendlePtOracleLib.getPtToAssetRate(IPMarket(_oracle.market()), _TWAP_DURATION);
+
+        (, int256 answer, , , ) = _oracle.latestRoundData();
+        uint256 value = uint256(answer);
+
+        assertEq(value, pendleAMMPrice);
+    }
+
+    function test_AfterMaturity_Success() public {
+        // Adavnce to the PT maturity
+        vm.warp(_oracle.maturity());
+
+        uint256 pendleAMMPrice = PendlePtOracleLib.getPtToAssetRate(IPMarket(_oracle.market()), _TWAP_DURATION);
+        (, int256 answer, , , ) = _oracle.latestRoundData();
+        uint256 value = uint256(answer);
+
+        assertEq(value, pendleAMMPrice);
+        assertEq(value, 1 ether);
+    }
+
+    function test_HackRemove_Success(uint256 slash) public {
+        slash = bound(slash, 1, BASE_18);
+        // Remove part of the SY backing collateral to simulate a hack
+        IERC20 weETH = IERC20(address(_oracle.asset()));
+        uint256 prevBalance = weETH.balanceOf(_oracle.sy());
+        uint256 postBalance = (prevBalance * slash) / BASE_18;
+        deal(address(weETH), _oracle.sy(), postBalance);
+
+        uint256 lowerBound = _economicLowerBound(_MAX_IMPLIED_RATE, _oracle.maturity());
+        (, int256 answer, , , ) = _oracle.latestRoundData();
+        uint256 value = uint256(answer);
+
+        assertLe(value, (lowerBound * slash) / BASE_18);
+        if (slash > 0) assertGe(value, (lowerBound * (slash - 1)) / BASE_18);
+    }
+
+    function test_HackExpand_Success(uint256 expand) public {
+        expand = bound(expand, BASE_18, BASE_18 * 1e7);
+        // Remove part of the SY backing collateral to simulate a hack
+        IERC20 weETH = IERC20(address(_oracle.asset()));
+        uint256 prevBalance = weETH.balanceOf(_oracle.sy());
+        uint256 postBalance = (prevBalance * expand) / BASE_18;
+        deal(address(weETH), _oracle.sy(), postBalance);
+
+        uint256 lowerBound = _economicLowerBound(_MAX_IMPLIED_RATE, _oracle.maturity());
+        (, int256 answer, , , ) = _oracle.latestRoundData();
+        uint256 value = uint256(answer);
+
+        assertEq(value, lowerBound);
+    }
+}
