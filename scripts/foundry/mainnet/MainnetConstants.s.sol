bc28d18d41604c044c7e79eb73d90b7c4896f494
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2024-04-26 19:48:59 +0200

    deploy market

--- scripts/foundry/mainnet/MainnetConstants.s.sol
+++ scripts/foundry/mainnet/MainnetConstants.s.sol
@@ -31,6 +31,7 @@ contract MainnetConstants is CommonUtils {
     address constant USDA = 0x0000206329b97DB379d5E1Bf586BbDB969C63274;
     address constant EZETH = 0xbf5495Efe5DB9ce00f80364C8B423567e58d2110;
     address constant PTWeETH = 0xc69Ad9baB1dEE23F4605a82b3354F8E40d1E5966;
+    address constant PTUSDe = 0xa0021EF8970104c2d008F38D92f115ad56a9B8e1;
     address constant RSETH = 0xA1290d69c65A6Fe4DF752f95823fae25cB99e5A7;
     address constant GTETHPRIME = 0x2371e134e3455e0593363cBF89d3b6cf53740618;
     address constant GTUSDCPRIME = 0xdd0f28e19C1780eb6396170735D45153D261490d;
@@ -41,9 +42,9 @@ contract MainnetConstants is CommonUtils {
 
     address constant EZETH_ETH_ORACLE = 0xF4a3e183F59D2599ee3DF213ff78b1B3b1923696;
     address constant RSETH_ETH_ORACLE = 0xA736eAe8805dDeFFba40cAB8c99bCB309dEaBd9B;
-    // TODO: this one needs to be updated and changed
     address constant WEETH_USD_ORACLE = 0xdDb6F90fFb4d3257dd666b69178e5B3c5Bf41136;
     address constant PTEETH_WEETH_ORACLE = 0xC9dfD5c18F12a3BA6293001700810602efe0c45B;
+    address constant USDE_USD_ORACLE = 0xbC5FBcf58CeAEa19D523aBc76515b9AEFb5cfd58;
 
     address constant CHAINLINK_EURA_EUR_ORACLE = 0xb4d5289C58CE36080b0748B47F859D8F50dFAACb;
     address constant CHAINLINK_ETH_USD_ORACLE = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
--- scripts/foundry/mainnet/PTUSDeOracle.s.sol
+++ scripts/foundry/mainnet/PTUSDeOracle.s.sol
@@ -3,11 +3,24 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
+import { IMorpho, MarketParams } from "../../../../contracts/interfaces/external/morpho/IMorpho.sol";
+import { IMorphoOracle } from "../../../../contracts/interfaces/external/morpho/IMorphoChainlinkOracleV2.sol";
+import { IMorphoChainlinkOracleV2Factory } from "borrow-contracts/interfaces/external/morpho/IMorphoChainlinkOracleV2Factory.sol";
 import { MorphoFeedPTUSDe } from "borrow-contracts/oracle/morpho/mainnet/MorphoFeedPTUSDe.sol";
-import "utils/src/CommonUtils.sol";
+import { MainnetConstants } from "./MainnetConstants.s.sol";
+import "../../../lib/utils/src/Constants.sol";
 import { IAccessControlManager } from "borrow-contracts/interfaces/IAccessControlManager.sol";
+import { StdCheats, StdAssertions } from "forge-std/Test.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IERC4626 } from "borrow-contracts/interfaces/external/IERC4626.sol";
 
-contract PTweETHOracleDeploy is Script, CommonUtils {
+contract PTUSDeOracleDeploy is MainnetConstants, Script, StdCheats, StdAssertions {
+    MarketParams public params;
+    bytes public emptyData;
+    uint256 constant BASE_DEPOSIT_AMOUNT = 10 ether;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
@@ -20,14 +33,52 @@ contract PTweETHOracleDeploy is Script, CommonUtils {
         uint256 _MAX_IMPLIED_RATE = 0.5 ether;
         // end TODO
 
-        MorphoFeedPTUSDe oracle = new MorphoFeedPTUSDe(
+        MorphoFeedPTUSDe priceFeed = new MorphoFeedPTUSDe(
             IAccessControlManager(address(coreBorrow)),
             _MAX_IMPLIED_RATE,
             _TWAP_DURATION
         );
-        (, int256 answer, , , ) = oracle.latestRoundData();
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
         console.log("oracle value ", uint256(answer));
-        console.log("Successfully deployed PT-weETH: ", address(oracle));
+        console.log("Successfully deployed PT-weETH: ", address(priceFeed));
+
+        MarketParams memory params;
+        bytes memory emptyData;
+
+        params.collateralToken = PTUSDe;
+        params.lltv = LLTV_86;
+        params.irm = IRM_MODEL;
+        params.loanToken = USDA;
+
+        // PT USDe market
+        params.oracle = address(
+            IMorphoChainlinkOracleV2Factory(MORPHO_ORACLE_FACTORY).createMorphoChainlinkOracleV2(
+                IERC4626(address(0)),
+                1,
+                AggregatorV3Interface(address(priceFeed)),
+                AggregatorV3Interface(USDE_USD_ORACLE),
+                IERC20Metadata(PTUSDe).decimals(),
+                IERC4626(address(0)),
+                1,
+                AggregatorV3Interface(address(0)),
+                AggregatorV3Interface(address(0)),
+                IERC20Metadata(params.loanToken).decimals(),
+                hex""
+            )
+        );
+
+        {
+            uint256 price = IMorphoOracle(params.oracle).price();
+            assertApproxEqRel(price, 0.9 ether * 1 ether, 0.01 ether);
+        }
+
+        IMorpho(MORPHO_BLUE).createMarket(params);
+
+        IERC20(USDA).approve(MORPHO_BLUE, 20 ether);
+        IMorpho(MORPHO_BLUE).supply(params, 1 ether, 0, deployer, emptyData);
+        IERC20(params.collateralToken).approve(MORPHO_BLUE, BASE_DEPOSIT_AMOUNT);
+        IMorpho(MORPHO_BLUE).supplyCollateral(params, BASE_DEPOSIT_AMOUNT, deployer, emptyData);
+        IMorpho(MORPHO_BLUE).borrow(params, 1 ether, 0, deployer, deployer);
 
         vm.stopBroadcast();
     }
